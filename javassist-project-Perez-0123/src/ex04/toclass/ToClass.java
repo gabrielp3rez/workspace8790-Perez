a5568607f1d4937a66ff08db2536d88131967a81
Gabriel Perez <abie.perez@gmail.com>
2020-01-29 15:11:19 -0600

    Added documentation

--- javassist-project-Perez-0123/src/ex04/toclass/ToClass.java
+++ javassist-project-Perez-0123/src/ex04/toclass/ToClass.java
@@ -10,10 +10,11 @@ import javassist.CtClass;
 import javassist.CtConstructor;
 import javassist.NotFoundException;
 
-public class ToClass {
-	
+public class ToClass
+{	
 	private static Scanner scan;
 	private static final String PKG_NAME = "target" + ".";
+	
 	public static void main(String[] args)
 	{
 		scan = new Scanner(System.in);
@@ -21,31 +22,42 @@ public class ToClass {
 		validClassNames.add("CommonComponentB");
 		validClassNames.add("CommonServiceA");
 		
+		// Get input
 		String className = getInput(validClassNames);
-		String methodName = className;
-		System.out.println(methodName);
+
 		try {
+			// Get class pool and class
 			ClassPool cp = ClassPool.getDefault();
 			CtClass cc = cp.get(PKG_NAME + className);
 			
+			// Get constructor to be modified
 			CtConstructor declaredConstructor = cc.getDeclaredConstructor(new CtClass[0]);
 		
+			// Block to insert
 			String block = "{\n" 
 					+ "\tSystem.out.println(\"id: \" + id);\n"
 					+ "\tSystem.out.println(\"year: \" + year);\n"
 					+ "}";
 			
 			System.out.println("[DBG] BLOCK: " + block);
+			
+			// Insert block in constructor
 			declaredConstructor.insertAfter(block);
+			
+			// Create instance of class to call constructor
 			Class<?> rClass = cc.toClass();
 			Object inst = rClass.newInstance();
 			
 		} catch (NotFoundException | CannotCompileException | InstantiationException | IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	/*
+	 * Gets the input from the user
+	 * @param validClassNames
+	 * @return the class name
+	 */
 	public static String getInput(List<String> validClassNames)
 	{
 		boolean valid = false;
@@ -53,13 +65,17 @@ public class ToClass {
 		do
 		{
 			System.out.print("Enter a Class Name (CommonComponentB or CommonServiceA): ");
+			// Get the inputs
 			String[] arguments = scan.nextLine().split(",\\s*");
+			
+			// Should only be one input
 			if (arguments.length != 1)
 			{
 				System.out.println("[WRN] Invalid Input");
 			}
 			else
 			{
+				// Valid class names are CommonComponentB or CommonServiceA
 				if (!validClassNames.contains(arguments[0]))
 				{
 					System.out.println("[WRN] Invalid Input");
